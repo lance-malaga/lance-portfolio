635030592a246874ad98fb2b223a612fd5e36760
lance-malaga <lmalaga@my.bcit.ca>
2024-01-26 13:02:45 -0800

    add shapes data and background images

---
+++ data/shapes.js
@@ -0,0 +1,61 @@
+import shape01 from '@/public/images/home/shape-1.svg'
+import shape02 from '@/public/images/home/shape-2.svg'
+import shape03 from '@/public/images/home/shape-3.svg'
+import shape04 from '@/public/images/home/shape-4.svg'
+import shape05 from '@/public/images/home/shape-5.svg'
+import shape06 from '@/public/images/home/shape-6.svg'
+import shape07 from '@/public/images/home/shape-7.svg'
+import shape08 from '@/public/images/home/shape-8.svg'
+import shape09 from '@/public/images/home/shape-9.svg'
+import shape10 from '@/public/images/home/shape-10.svg'
+import sample01 from '@/public/images/home/sample-1.svg'
+import sample02 from '@/public/images/home/sample-2.svg'
+import sample03 from '@/public/images/home/sample-3.svg'
+import sample04 from '@/public/images/home/sample-4.svg'
+
+export const shapes = [
+    {
+        id: 1,
+        shape: shape01
+    },
+    {
+        id: 2,
+        shape: shape02,
+        sample: sample01
+    },
+    {
+        id: 3,
+        shape: shape03
+    },
+    {
+        id: 4,
+        shape: shape04,
+    },
+    {
+        id: 5,
+        shape: shape05,
+        sample: sample02
+    },
+    {
+        id: 6,
+        shape: shape06,
+    },
+    {
+        id: 7,
+        shape: shape07,
+        sample: sample03,
+    },
+    {
+        id: 8,
+        shape: shape08,
+    },
+    {
+        id: 19,
+        shape: shape09,
+        sample: sample04
+    },
+    {
+        id: 10,
+        shape: shape10
+    },
+];
