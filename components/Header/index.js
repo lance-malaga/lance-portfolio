a6079726da3bc6f550a957b31aa4f7f1c504183a
lance-malaga <lmalaga@my.bcit.ca>
2024-01-28 21:35:10 -0800

    add link to header logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,16 +3,19 @@ import styles from "./Header.module.scss"
 
 import logo from "@/public/logo.svg"
 import menuIcon from "@/public/icons/menu-icon.svg"
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <Image
-                src={logo}
-                alt="logo"
-                width={84}
-                height={32}
-            />
+            <Link href={'/'}>
+                <Image
+                    src={logo}
+                    alt="logo"
+                    width={84}
+                    height={32}
+                />
+            </Link>
             <Image
                 src={menuIcon}
                 alt="menu-icon"
