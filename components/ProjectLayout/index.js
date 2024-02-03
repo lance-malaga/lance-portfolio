151aa25bfcb3aa1860eeca3a0b0fb11c02befc07
lance-malaga <lmalaga@my.bcit.ca>
2024-02-03 13:28:48 -0800

    update link out button

--- components/ProjectLayout/index.js
+++ components/ProjectLayout/index.js
@@ -2,6 +2,9 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from './ProjectLayout.module.scss'
 
+// assets
+import linkIcon from '@/public/icons/link-out-icon.svg'
+
 export default function ProjectLayout({project}) {
     return (
         <div className={styles.project__layout}>
@@ -12,15 +15,19 @@ export default function ProjectLayout({project}) {
                 {project.desc}
             </p>
             { project.link && 
-                <p className={styles.link__out}>
-                    TRY IT OUT:{' '}
-                    <Link href={project.link} target='_blank'>
-                        {project.title}
-                    </Link>
-                </p>
+                <Link href={project.link} target='_blank' className={styles.link__out}>
+                    <Image
+                        src={linkIcon}
+                        alt={'link-out-icon'}
+                        width={25}
+                        height={25}
+                        className={styles.gallery_img}
+                    />
+                    <p>TRY IT OUT</p>
+                </Link>
             }
             <div className={styles.gallery}>
-                {project.gallery.map((data, index) => (
+                {project.gallery && project.gallery.map((data, index) => (
                     <button 
                         key={index} 
                         className={styles.overlay}
