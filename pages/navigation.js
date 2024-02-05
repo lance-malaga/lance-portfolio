585d5cf1e0bf299044a74ee141008f987d7e7797
lance-malaga <lmalaga@my.bcit.ca>
2024-02-05 02:50:31 -0800

    add navigation page

---
+++ pages/navigation.js
@@ -0,0 +1,74 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '@/styles/Navigation.module.scss'
+
+// components
+import CustomHead from '@/components/CustomHead'
+import Header from '@/components/Header'
+
+// assets
+import backgroundMenu from '@/public/images/background/background-menu.png'
+import navbarShapes from '@/public/images/navbar-shapes.svg'
+import linkIcon from '@/public/icons/link-out-icon.svg'
+
+export default function Navigation() {
+    const navList = [
+        {name: 'Home', link: '/'},
+        {name: 'Work', link: '/work/development'},
+        {name: 'About', link: '/about'},
+        {name: 'Contact', link: '/contact'},
+    ];
+
+    return (
+        <div className={styles.navigation__container}>
+            <Image
+				src={backgroundMenu}
+				alt='background-img-navigation'
+				width={"auto"}
+				height={"auto"}
+				className={styles.background_img}
+			/>
+			<CustomHead title={'Navigation'}/>
+			<Header/>
+            <main>
+                <Image
+                    src={navbarShapes}
+                    alt='navbar-shapes'
+                    width={"auto"}
+                    height={"auto"}
+                />
+                <div className={styles.content__container}>
+                    <div className={styles.nav_list__container}>
+                        {navList.map((item, index) => (
+                            <Link href={item.link}>
+                                <h1>
+                                    <span>0{index + 1} </span>{item.name}
+                                </h1>
+                            </Link>
+                        ))}
+                    </div>
+                    <div className={styles.link__container}>
+                        <Link href='https://www.linkedin.com/in/lance-matsuo-malaga/'>
+                            <Image
+                                src={linkIcon}
+                                alt='link-icon'
+                                width={"auto"}
+                                height={"auto"}
+                            />
+                            <span>linkedin</span>
+                        </Link>
+                        <Link href='https://www.instagram.com/lancemalaga/'>
+                            <Image
+                                src={linkIcon}
+                                alt='link-icon'
+                                width={"auto"}
+                                height={"auto"}
+                            />
+                            <span>instagram</span>
+                        </Link>
+                    </div>
+                </div> 
+            </main>
+        </div>
+    )
+}
