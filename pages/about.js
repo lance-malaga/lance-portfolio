f35ca2110828e3e2781a96afc9a6a5e612c86a58
lance-malaga <lmalaga@my.bcit.ca>
2024-02-05 02:50:07 -0800

    add about and contact pages

---
+++ pages/about.js
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import styles from '@/styles/About.module.scss'
+
+// components
+import CustomHead from '@/components/CustomHead'
+import Header from '@/components/Header'
+
+// assets
+import backgroundAbout from '@/public/images/background/background-about.png'
+import profile_img from '@/public/images/profile.svg'
+import download_icon from '@/public/icons/download-icon.svg'
+
+export default function About() {
+    return (
+        <div className={styles.about__container}>
+            <Image
+				src={backgroundAbout}
+				alt='background-img-about'
+				width={"auto"}
+				height={"auto"}
+				className={styles.background_img}
+			/>
+			<CustomHead title={'About'}/>
+			<Header/>
+            <main>
+                <Image
+                    src={profile_img}
+                    alt='profile'
+                    width={"auto"}
+                    height={"auto"}
+                    className={styles.profile_img}
+                />
+                <div>
+                    <h1>About</h1>
+                    <p>
+                        Hello, I'm Lance—a versatile professional with a proven track record in crafting impactful websites, mobile applications, and designs. My journey through Graphic Design, UI/UX Design, Frontend Development and Architecture has equipped me with a diverse skill set. I'm always eager to learn new things, and this passion for continuous growth fuels my ability to approach challenges from unique perspectives, fostering creativity and innovation in both development and design. I thrive on delivering solutions that not only meet but exceed the expectations of my clients.
+                    </p>
+                    <button>
+                        <Image
+                            src={download_icon}
+                            alt='download-icon'
+                            width={18}
+                            height={18}
+                        />
+                        <span>resume</span>
+                    </button>
+                </div>
+            </main>
+        </div>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+import styles from '@/styles/Contact.module.scss'
+
+// components
+import CustomHead from '@/components/CustomHead'
+import Header from '@/components/Header'
+
+// assets
+import backgroundContact from '@/public/images/background/background-contact.png'
+import linkIcon from '@/public/icons/link-out-icon.svg'
+
+export default function Contact() {
+    return (
+        <div className={styles.contact__container}>
+            <Image
+				src={backgroundContact}
+				alt='background-img-contact'
+				width={"auto"}
+				height={"auto"}
+				className={styles.background_img}
+			/>
+			<CustomHead title={'Contact'}/>
+			<Header/>
+            <main>
+                <h1>Contact</h1>
+                <div className={styles.content__container}>
+                    <div className={styles.mail__container}>
+                        <h4>Mail</h4>
+                        <p>lance.matsuo@gmail.com</p>
+                    </div>
+                    <div className={styles.social__container}>
+                        <h4>Social Media</h4>
+                        <div className={styles.link__container}>
+                            <button>
+                                <Image
+                                    src={linkIcon}
+                                    alt='link-icon'
+                                    width={"auto"}
+                                    height={"auto"}
+                                />
+                                <span>linkedin</span>
+                            </button>
+                            <button>
+                                <Image
+                                    src={linkIcon}
+                                    alt='link-icon'
+                                    width={"auto"}
+                                    height={"auto"}
+                                />
+                                <span>instagram</span>
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+            </main>
+        </div>
+    )
+}
