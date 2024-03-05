ce7a5d7345f60531f550dea85a9e216aae79d774
lance-malaga <lmalaga@my.bcit.ca>
2024-03-05 14:56:47 -0800

    update portrait layout

--- components/ProjectPortraitLayout/index.js
+++ components/ProjectPortraitLayout/index.js
@@ -4,6 +4,7 @@ import styles from './ProjectPortraitLayout.module.scss'
 
 // assets
 import closeIcon from '@/public/icons/close-btn.svg'
+import linkIcon from '@/public/icons/link-out-icon.svg'
 
 export default function ProjectPortraitLayout({category , project}) {
     return (
@@ -37,7 +38,37 @@ export default function ProjectPortraitLayout({category , project}) {
                     <p className={styles.description}>
                         {project.desc}
                     </p>
+                    { project.link && 
+                        <Link href={project.link} target='_blank' className={styles.link__out}>
+                            <Image
+                                src={linkIcon}
+                                alt={'link-out-icon'}
+                                width={25}
+                                height={25}
+                            />
+                            <p>TRY IT OUT</p>
+                        </Link>
+                    }
                 </div>
+            </div>
+            <div 
+                className={styles.gallery}
+                style={{
+                    gap: project.title === 'YORI' ? '20px' : null,
+                }}
+            >
+                {project.gallery && project.gallery.map((data, index) => (
+                    <Image 
+                        src={data}
+                        alt={'gallery-img'+`${index}`}
+                        width={1000}
+                        height={700}
+                        key={index}
+                        style={{
+                            width: project.title === 'YORI' ? 'auto' : null,
+                        }}
+                    />
+                ))}
             </div>
         </div>
     )
