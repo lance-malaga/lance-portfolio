90eccca1e606acddd9c84c4fabf6f7ee24ddb58a
lance-malaga <lmalaga@my.bcit.ca>
2024-11-08 02:41:29 -0800

    add ux/ui data

---
+++ data/work_ux_ui.js
@@ -0,0 +1,34 @@
+export const work_ux_ui = [
+    {
+        title: `REVERIE`,
+        slug: 'reverie',
+        subtitle: 'FIGMA PROJECT',
+        grid_img: '/images/work/ux-ui/grid/work01-reverie.svg',
+        banner: '/images/work/ux-ui/work01-reverie.png',
+        programs: 'FIGMA',
+        desc: 'Designed in Figma, the Reverie website combines elegance with functionality, using a vibrant yellow theme to create a bold, upscale feel. The layout enhances the user experience with intuitive sections for collections, best sellers, and personalized assistance. Each design element is crafted to create a visually appealing, curated shopping journey, with a focus on making the customer feel valued and stylish.',
+        link: `https://www.figma.com/proto/yThDLoCNNWQVi6B74c7rzQ/Interface---Final-Redesign?node-id=108-1069&node-type=frame&t=o5E1QNzTgDLXQ1Mg-1&scaling=scale-down-width&content-scaling=fixed&page-id=98%3A1196&starting-point-node-id=108%3A1069`,
+        gallery: [
+            '/images/work/ux-ui/work01-reverie/gallery-01.png',
+            '/images/work/ux-ui/work01-reverie/gallery-02.png',
+            '/images/work/ux-ui/work01-reverie/gallery-03.png',
+            '/images/work/ux-ui/work01-reverie/gallery-04.png',
+        ]
+    },
+    {
+        title: `GAIA`,
+        slug: 'gaia',
+        subtitle: 'FIGMA PROJECT',
+        grid_img: '/images/work/ux-ui/grid/work02-gaia.svg',
+        banner: '/images/work/ux-ui/work02-gaia.png',
+        programs: 'FIGMA',
+        desc: `Designed in Figma, this website for Gaia Co. captures the artistry of pottery through an elegant and inviting layout. I used a palette of earthy tones to reflect the brand's connection to natural materials and craftsmanship. Each element of the design was carefully chosen to evoke warmth and creativity, making the site both visually appealing and informative.`,
+        link: `https://www.figma.com/proto/AdPjgwHBfzGfq516wPoOqL/Gaia-Co.-%E2%80%93-SET-F?node-id=1020-16666&node-type=frame&t=mmPVNRY1ztu4lexM-1&scaling=min-zoom&content-scaling=fixed&page-id=785%3A14975&starting-point-node-id=785%3A16309`,
+        gallery: [
+            '/images/work/ux-ui/work02-gaia/gallery-01.png',
+            '/images/work/ux-ui/work02-gaia/gallery-02.png',
+            '/images/work/ux-ui/work02-gaia/gallery-03.png',
+            '/images/work/ux-ui/work02-gaia/gallery-04.png',
+        ],
+    },
+];
