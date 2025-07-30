c2b387563af2bdd23fa78bbfb25cee61ab5be038
lance-malaga <lmalaga@my.bcit.ca>
2025-07-29 20:52:29 -0700

    add new home layout

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '@/styles/Home.module.scss'
 import { motion } from 'framer-motion'
+import Marquee from 'react-fast-marquee'
 
 // components
 import Header from '@/components/Header'
 import CustomHead from '@/components/CustomHead'
 import ShapeAnimation from '@/components/ShapeAnimation'
+import Footer from '@/components/Footer'
+import FadeInOnView from '@/components/FadeInOnView'
+import CaseStudyItem from '@/components/CaseStudyItem'
 
 // assets
 import forwardIcon from '@/public/icons/forward-icon.svg'
-import backgroundHome from '@/public/images/background/background-home.png'
+import linkIcon from '@/public/icons/link-out-icon.svg'
+import asteriskIcon from '@/public/icons/asterisk-icon.svg'
+import circleArrow from '@/public/icons/circle-arrow.svg'
+import stripShapes from '@/public/images/home/strip-shapes.png'
+import stripShapes2 from '@/public/images/home/strip-shapes-2.png'
+import exploreMore from '@/public/images/home/explore-more.png'
+import backgroundHome from '@/public/images/background/background-home-new.png'
+import spurtBanner from "@/public/images/home/project-banners/spurt-banner.jpg"
+import hyggeBanner from "@/public/images/home/project-banners/hygge-banner.jpg"
 
 export default function Home() {
 	return (
@@ -34,7 +46,7 @@ export default function Home() {
 					>
 						<h1>Lance Malaga</h1>
 						<h4>full stack developer, ux/ui & graphic designer.</h4>
-						<div className={styles.buttons__container}>
+						{/* <div className={styles.buttons__container}>
 							<Link href={`/work/development`}>
 								<Image
 									src={forwardIcon}
@@ -53,6 +65,15 @@ export default function Home() {
 								/>
 								<p>more about me</p>
 							</Link>
+						</div> */}
+						<div className={styles.buttons__container}>
+							<Link href={"#case-studies-section"}>
+								<Image
+									src={circleArrow}
+									alt={'forward-icon'}
+								/>
+								<p>case studies</p>
+							</Link>
 						</div>
 					</motion.div>
 					<motion.div 
@@ -64,6 +85,75 @@ export default function Home() {
 						<ShapeAnimation/>
 					</motion.div>
 				</main>
+				<FadeInOnView className={styles.marquee__container}>
+					<Marquee gradient={false} speed={100} delay={1.5}>
+						<Image
+							src={stripShapes}
+							alt={"strip-shapes"}
+						/>
+						<h1>WELCOME TO MY STUDIO</h1>
+						<Image
+							src={stripShapes2}
+							alt={"strip-shapes"}
+						/>
+						<h1>WELCOME TO MY STUDIO</h1>
+					</Marquee>
+					<div id='case-studies-section'></div>
+				</FadeInOnView>
+				<FadeInOnView className={styles.case_study__container}>
+					<div className={styles.case_study__header}>
+						<Image src={asteriskIcon} alt='asterisk-icon'/>
+						<h2>CASE STUDIES</h2>
+					</div>
+					<div className={styles.case_study__list}>
+						<Link href={'work/ux-ui-design/hygge'}>
+							<CaseStudyItem
+								image={hyggeBanner}
+								number={"01"}
+								title={"Hygge Design House"}
+								tags={["ux/ui", "web design", "branding"]}
+							/>
+						</Link>
+						<Link href={'work/development/spurt'}>
+							<CaseStudyItem
+								image={spurtBanner}
+								number={"02"}
+								title={"Spurt"}
+								tags={["mobile development", "ux/ui", "graphic design", "branding"]}
+							/>
+						</Link>
+					</div>
+				</FadeInOnView>
+				<FadeInOnView className={styles.explore__container}>
+					<Image
+						src={exploreMore}
+						alt='explore-more-img'
+						className={styles.explore_img}
+					/>
+					<div className={styles.link__container}>
+						<Link href='/work/graphic-design'>
+							<Image
+								src={linkIcon}
+								alt='link-icon'
+								width={"auto"}
+								height={"auto"}
+							/>
+							<span>see other work</span>
+						</Link>
+						<Link href='/contact' >
+							<Image
+								src={linkIcon}
+								alt='link-icon'
+								width={"auto"}
+								height={"auto"}
+							/>
+							<span>contact me</span>
+						</Link>
+					</div>
+				</FadeInOnView>
+				<FadeInOnView className={styles.footer__container}>
+					<Footer/>
+				</FadeInOnView>
 			</div>
 		</div>
 	)
